ad830b1c153d7252cdea4844de7dd3754a598296
Alamgirhossann <alamgirhossann@gmail.com>
2023-05-30 17:24:43 +0600

    setup ["DONE"] express, cors,mongoose, parser

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app:Application  = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req: Request, res:Response) => {
   res.send('working application')
