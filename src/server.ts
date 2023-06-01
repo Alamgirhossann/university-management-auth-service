cdf5cedc95e7726801afd5865071ce76e889f12b
Alamgirhossann <alamgirhossann@gmail.com>
2023-06-01 16:08:40 +0600

    setup[done] user interface and model

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
-            console.log(`Application app listening on port ${config.port}`)
-          })
-        console.log('database connected')
-    } catch (error) {
-        console.log('database fail to connect', error)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`)
+    })
+    console.log('database connected')
+  } catch (error) {
+    console.log('database fail to connect', error)
+  }
 }
 
-bootstrap();
+bootstrap()
